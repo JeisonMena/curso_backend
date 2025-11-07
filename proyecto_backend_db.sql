c9039ba8fdb21fa24d9e3af53a436c53c97a04d3
Jeison Mena <m.jeison117@outlook.es>
2025-11-06 20:38:19 -0600

    update: Se actualiza el script de la DB

--- proyecto_backend_db.sql
+++ proyecto_backend_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-10-2025 a las 04:29:19
+-- Tiempo de generación: 07-11-2025 a las 03:37:34
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -24,6 +24,52 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `departamento`
+--
+
+CREATE TABLE `departamento` (
+  `id_departamento` int(11) NOT NULL,
+  `nombre` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `departamento`
+--
+
+INSERT INTO `departamento` (`id_departamento`, `nombre`) VALUES
+(1, 'Finanzas'),
+(2, 'Operaciones'),
+(3, 'Transformacion Digital'),
+(4, 'Recuersos Humanos'),
+(5, 'Tecnologias de Informacion y Comunicacion (TIC)'),
+(6, 'Gestion de Credito');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ticket`
+--
+
+CREATE TABLE `ticket` (
+  `id_ticket` int(11) NOT NULL,
+  `titulo` varchar(128) NOT NULL,
+  `descripcion` varchar(512) NOT NULL,
+  `prioridad` tinyint(1) NOT NULL DEFAULT 1 COMMENT '1=Baja, 2=Media, 3=Urgente',
+  `url_imagen` varchar(256) DEFAULT NULL,
+  `nombre` varchar(256) NOT NULL,
+  `cedula` varchar(64) NOT NULL,
+  `telefono` varchar(64) DEFAULT NULL,
+  `correo` varchar(64) DEFAULT NULL,
+  `id_departamento` int(11) NOT NULL,
+  `fecha` datetime NOT NULL,
+  `id_usuario_asignado` int(11) DEFAULT NULL,
+  `estado` tinyint(4) NOT NULL COMMENT '0=pendiente, 1=resuelto, 2= sin solucion',
+  `fecha_finalizado` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuario`
 --
 
@@ -31,38 +77,66 @@ CREATE TABLE `usuario` (
   `id_usuario` int(11) NOT NULL,
   `cedula` varchar(128) NOT NULL,
   `nombre` varchar(256) NOT NULL,
-  `clave` varchar(64) NOT NULL
+  `clave` varchar(64) NOT NULL,
+  `estado` tinyint(1) DEFAULT 1 COMMENT '0=inactivo, 1=activo, 2=eliminado'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`id_usuario`, `cedula`, `nombre`, `clave`) VALUES
-(1, '123456789', 'Jeison Mena Marin', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3'),
-(2, '987654321', 'Juan Perez Castro\r\n', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3'),
-(3, '1122', 'Juan Perez', 'f6e0a1e2ac41945a9aa7ff8a8aaa0cebc12a3bcc981a929ad5cf810a090e11ae'),
-(214, '2312321321', 'Smith Jhon', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3');
+INSERT INTO `usuario` (`id_usuario`, `cedula`, `nombre`, `clave`, `estado`) VALUES
+(1, '123456789', 'Jeison Mena Marin', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 1),
+(2, '987654321', 'Juan Perez Castro\r\n', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 1),
+(3, '1122', 'Juan Perez', 'f6e0a1e2ac41945a9aa7ff8a8aaa0cebc12a3bcc981a929ad5cf810a090e11ae', 1),
+(214, '2312321321', 'Smith Jhon', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 1),
+(215, '12345', 'JEISON MENA', '932f3c1b56257ce8539ac269d7aab42550dacf8818d075f0bdf1990562aae3ef', 1),
+(216, '123', 'jeison', 'ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f', 1);
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `departamento`
+--
+ALTER TABLE `departamento`
+  ADD PRIMARY KEY (`id_departamento`);
+
+--
+-- Indices de la tabla `ticket`
+--
+ALTER TABLE `ticket`
+  ADD PRIMARY KEY (`id_ticket`);
+
+--
 -- Indices de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id_usuario`);
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD UNIQUE KEY `cedula` (`cedula`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
+-- AUTO_INCREMENT de la tabla `departamento`
+--
+ALTER TABLE `departamento`
+  MODIFY `id_departamento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `ticket`
+--
+ALTER TABLE `ticket`
+  MODIFY `id_ticket` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=215;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=217;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
